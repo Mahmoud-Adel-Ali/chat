10af757380d47b8ffc248b8651b522b9aab7821c
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-02-26 18:55:35 -0800

    verify view done

---
+++ lib/features/auth/presentation/views/widgets/verify_phone_number_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:chat/constant.dart';
+import 'package:chat/core/utils/styles.dart';
+import 'package:chat/features/auth/presentation/views/widgets/verification_code_section.dart';
+import 'package:flutter/material.dart';
+
+class VerifyPhoneNumberViewBody extends StatelessWidget {
+  const VerifyPhoneNumberViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 130),
+        const Text("Enter the code that was send to ",
+            style: Styles.textstyle27),
+        Text(
+          "0123456789",
+          style: Styles.textstyle27.copyWith(
+            color: kthirdAuthPage,
+          ),
+        ),
+        const SizedBox(height: 130),
+        const VerificationCodeSection(),
+      ],
+    );
+  }
+}
