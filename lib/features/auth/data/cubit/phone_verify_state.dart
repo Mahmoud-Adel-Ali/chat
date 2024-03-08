fc057a4422b556a9969c3334e31c0ab2ed53f3ed
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-03-08 14:48:32 -0800

    create state of  phone and verify view

--- lib/features/auth/data/cubit/phone_verify_state.dart
+++ lib/features/auth/data/cubit/phone_verify_state.dart
@@ -1,3 +1,17 @@
-abstract class PhoneVerifyState{}
+abstract class PhoneVerifyState {}
 
-class PhoneVerifyInitial extends PhoneVerifyState{}
+class PhoneVerifyInitial extends PhoneVerifyState {}
+
+// phone view state
+class PhoneLoading extends PhoneVerifyState {}
+
+class PhoneLSuccess extends PhoneVerifyState {}
+
+class PhoneFailure extends PhoneVerifyState {}
+
+// verify view state
+class VerifyLoading extends PhoneVerifyState {}
+
+class VerifyLSuccess extends PhoneVerifyState {}
+
+class VerifyFailure extends PhoneVerifyState {}
